3a0a3a763063aa94c46005e5252ecf5ff1931aa4
Felix Steghofer <steghofe@fim.uni-passau.de>
2014-03-16 16:44:50 +0100

    eclipse crosscompile, some esc tests

---
+++ SERVOS/test_diff.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+i=$1
+j=$2
+
+echo $i=100 > /dev/servoblaster
+echo $j=100 > /dev/servoblaster
+sleep 2
+echo $i=110 > /dev/servoblaster
+echo $j=110 > /dev/servoblaster
+sleep 2
+echo $i=120 > /dev/servoblaster
+echo $j=120 > /dev/servoblaster
+sleep 2
+echo $i=130 > /dev/servoblaster
+echo $j=130 > /dev/servoblaster
+sleep 2
+echo $i=140 > /dev/servoblaster
+echo $j=140 > /dev/servoblaster
+sleep 2
+echo $i=150 > /dev/servoblaster
+echo $j=150 > /dev/servoblaster
+sleep 2
+echo $i=160 > /dev/servoblaster
+echo $j=160 > /dev/servoblaster
+sleep 2
+echo $i=170 > /dev/servoblaster
+echo $j=170 > /dev/servoblaster
+sleep 1
